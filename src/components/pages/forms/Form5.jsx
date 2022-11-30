883188c8f1ca6874368b8784d922f42acb7c8d03
Suma-ReactDev <k.sumakumari@gmail.com>
2022-11-30 17:55:46 +0530

    CR done for form5 using spring

--- src/components/pages/forms/Form5.jsx
+++ src/components/pages/forms/Form5.jsx
@@ -1,8 +1,15 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
 import { Button } from './Form3';
-import Card from '../../helpers/Card'
+import Card from '../../helpers/Card';
+import axios from 'axios';
 const Form5 = () => {
   const [employees, setEmployees] = useState([]);
+  const [formData, setFormData] = useState({
+    firstName:'',
+    lastName:'',
+    emailId:''
+  })
+  const baseUrl = `http://localhost:8082/api/v1/employees`;
   const th= 'p-3 m-2 border-white border-seperate border-2 ';
   const td ='p-2 m-2';
   const getEmployees=async ()=>{
@@ -14,42 +21,60 @@ const Form5 = () => {
       }
     });
     const data=await response.json()
-    console.log(data)
     if (data){
-      console.log('Data is present')
       setEmployees(data)
     }
-    console.log(employees)
   }
   useEffect(()=>{
     getEmployees()
-  },[])
-
+  },[employees])
+  const onChange = (e) =>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
   const submitHandler = (e) =>{
     e.preventDefault();
-
+    axios.post(baseUrl,formData)
+    // const data = JSON.stringify(formData)
+    // const postFormData = async () =>{
+    //   const response = fetch('http://localhost:8082/api/v1/employees',
+    //   {
+    //     method:'POST',
+    //     headers:{
+    //       "Content-Type":"application/json"
+    //     },
+    //     body:data
+    //   });
+    //   console.log(response)
+    // }
+    // postFormData()
+      setFormData({...formData,emailId:'',firstName:'',lastName:''})
   }
   return (
     <Card className={`bg-sky-700`}>
-      
       <form className=' grid w-1/2 mx-auto m-2 bg-lime-200 p-2' onSubmit={submitHandler} >
         <input 
           type='text'
           placeholder='First Name'
           name='firstName'
+          value={formData.firstName}
           className='capitalize m-1 p-1 rounded'
+          onChange={onChange}
         />
         <input 
           type='text'
           placeholder='Last Name'
           name='lastName'
+          value={formData.lastName}
           className='capitalize m-1 p-1 rounded'
+          onChange={onChange}
         />
         <input 
           type='text'
           placeholder='Email'
-          name='email'
-          className='capitalize m-1 p-1 rounded'
+          name='emailId'
+          value={formData.emailId}
+          className=' m-1 p-1 rounded'
+          onChange={onChange}
         />
         <Button 
           type='submit'
